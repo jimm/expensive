54000acc5c7c4332ae402a8e077bd18cf272c7b8
Jim Menard <jim.menard@chloeandisabel.com>
2015-10-02 14:25:21 -0400

    Elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Expensive.Mixfile do
   def project do
     [app: :expensive,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
