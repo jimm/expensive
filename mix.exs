bd2cd64b3b1f204759f6c74e1b2f8bd90dd25b1f
Jim Menard <jim.menard@chloeandisabel.com>
2015-12-28 10:48:02 -0500

    broader version dep number

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Expensive.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:csvlixir, "~> 2.0.2"}]
+     {:csvlixir, "~> 2.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
